edc0f8a05a18b33848da525d28f5a6282a9a0879
PhamViet0312 <Vietpham31204@gmail.com>
2024-12-18 20:58:16 +0700

    render danh mục phim

---
+++ ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Do_An.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do_An.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly MovieContext _context;
+
+        public MenuViewComponent(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbMenus
+                .Where(m => m.IsActive == true).ToList();
+
+            return await Task.FromResult(View(items));
+        }
+    }
+}
---
+++ ViewComponents/MovieCategoryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Do_An.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do_An.ViewComponents
+{
+    public class MovieCategoryViewComponent : ViewComponent
+    {
+        private readonly MovieContext _context;
+
+        public MovieCategoryViewComponent(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbCategoryMovies
+                .Where(m => m.IsActive == true).ToList();
+
+            return await Task.FromResult(View(items));
+        }
+    }
+}
